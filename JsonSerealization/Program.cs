91c7bca3abb8145e87081e84a5ec46b372294760
beboantonio <beboantonio1@hotmail.com>
2021-06-21 06:30:53 -0300

    first commit

---
+++ JsonSerealization/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.Json;               // contains all entry points and main types
+using System.Text.Json.Serialization; //contains attributes and APIs for advanced scenarios and customization specific to serialization and deserialization. 
+
+namespace JsonSerealization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var weatherForecast = new WeatherForecast()
+            {
+                Date = DateTime.Parse("2019-08-01"),
+                TemperatureCelsius = 25,
+                Summary = "Hot"
+            };
+
+            // Write .NET objects as Json (Serialize)
+            var jsonString = JsonSerializer.Serialize(weatherForecast);
+
+            //The json output is minified (Whitespace, indentation and new-line caracters are removed) by default
+            Console.WriteLine(jsonString);
+        }
+    }
+}
